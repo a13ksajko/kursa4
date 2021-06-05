289b925db9e33915420b24244cb332cab9f6f3e1
LostGhost1 <askbefore@mail.ru>
2021-06-05 14:59:23 +0300

    Revert "Other socket stuff"
    This reverts commit 2af6e068

--- src/main/java/com/mechanitis/demo/quadrapassel/ServerScene.java
+++ src/main/java/com/mechanitis/demo/quadrapassel/ServerScene.java
@@ -9,8 +9,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class ServerScene extends Scene {
-    TextField ServerHostnameDesc = new TextField();
-    TextField ServerHostname = new TextField();
     TextField ServerPortDesc = new TextField();
     TextField ServerPort = new TextField();
     Button connect = new Button();
@@ -18,20 +16,16 @@ public class ServerScene extends Scene {
     public ServerScene(Parent root,Quadrapassel_App app) {
         super(root);
         this.app=app;
-        ServerHostnameDesc.setText("IP Address to bind to:");
-        ServerHostnameDesc.setEditable(false);
-        ServerPortDesc.setText("Port to listen on:");
+        ServerPortDesc.setText("Enter the server's port:");
         ServerPortDesc.setEditable(false);
         connect.setText("Host");
         connect.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                 app.ServerMode(ServerHostname.getText(),Integer.decode(ServerPort.getText()));
+                app.ServerMode(Integer.decode(ServerPort.getText()));
             }
         });
         VBox rootpane = (VBox) root;
-        rootpane.getChildren().add(ServerHostnameDesc);
-        rootpane.getChildren().add(ServerHostname);
         rootpane.getChildren().add(ServerPortDesc);
         rootpane.getChildren().add(ServerPort);
         rootpane.getChildren().add(connect);
